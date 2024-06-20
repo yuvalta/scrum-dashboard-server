0e57f9315f2e4225a7e3baa0b1d86edcd699d757
Yuval Tamir <yuval.tamir@zadarastorage.com>
2024-06-20 08:36:18 +0300

    001 Calculating time in each status in days

--- jira_manager.py
+++ jira_manager.py
@@ -15,14 +15,10 @@
 
 from jira import JIRA
 from dotenv import load_dotenv
+from common_functions import days_between_dates
 import os
-import json
-import sys
 
 load_dotenv()
-
-# TODO: calculate the time spent on each status in days
-jira = JIRA('https://zadara.atlassian.net', basic_auth=(os.getenv('JIRA_USERNAME'), os.getenv('JIRA_API_KEY')))
 
 
 class JiraManager(object):
@@ -31,19 +27,34 @@ class JiraManager(object):
                          basic_auth=(os.getenv('JIRA_USERNAME'), os.getenv('JIRA_API_KEY')))
 
     def get_time_in_status_for_ticket(self, ticket_key):
+        """
+        Get the time in each status for a specific ticket
+        Converting -
+            Triaging -> Closed on 2024-06-19T09:20:53.760+0300
+            Backlog -> Triaging on 2024-06-17T13:12:19.063+0300
+            Waiting for More Info -> Backlog on 2024-06-17T10:31:59.750+0300
+            Backlog -> Waiting for More Info on 2024-06-16T10:35:20.825+0300
+
+        to -
+            {"Waiting for More Info": 1, "Backlog": 1, "Triaging": 2}
+
+        :param ticket_key: The ticket key (e.g. NK-1234)
+        :return: A dictionary with the time in each status
+        """
         ticket = self.jira.issue(ticket_key, expand='changelog')
         statues_changes_list = []
 
         for history in ticket.changelog.histories:
             for item in history.items:
                 if item.field == 'status':
-                    print('%s -> %s on %s' % (item.fromString, item.toString, history.created))
                     statues_changes_list.append((item.fromString, item.toString, history.created))
 
-        statues_changes_list.sort(key=lambda x: x[2])
-
-        for status in range(1, len(statues_changes_list)):
-            print(statues_changes_list[status][2] - statues_changes_list[status - 1][2])
+        time_in_status_dict = {}
+        for i in range(len(statues_changes_list) - 1, 0, -1):
+            current_days_count = time_in_status_dict.get(statues_changes_list[i - 1][0], 0)
+            time_in_status_dict[statues_changes_list[i - 1][0]] = current_days_count + days_between_dates(
+                statues_changes_list[i - 1][2], statues_changes_list[i][2])
+        return time_in_status_dict
 
     def get_time_in_status_for_all_tickets_in_sprint_for_user(self, sprint_id, user_id):
         jql = f'project = NK AND Sprint = {sprint_id} AND assignee = {user_id}'  # or was assigned
